f9909841fd853482cd3014f25c89536c6f2edc9d
Gagan K Shetty <gagankshetty@gmail.com>
2018-04-18 14:12:50 +0530

    Update 5)Program for using ADC channel 0 and display value on the 7 segment.c

--- IOT/5)Program for using ADC channel 0 and display value on the 7 segment.c
+++ IOT/5)Program for using ADC channel 0 and display value on the 7 segment.c
@@ -37,15 +37,16 @@ int32_t main (void)
     char TEXT[16];               
     
     UNLOCKREG();
-    SYSCLK->PWRCON.XTL12M_EN = 1;     //Enable 12Mhz and set HCLK->12Mhz
-    SYSCLK->CLKSEL0.HCLK_S = 0;
+    //SYSCLK->PWRCON.XTL12M_EN = 1;     //Enable 12Mhz and set HCLK->12Mhz
+   // SYSCLK->CLKSEL0.HCLK_S = 0;
+    DrvSYS_Open(48000000);
     LOCKREG();
     Initial_panel();  // initialize LCD pannel
     clr_all_panel();  // clear LCD panel 
     print_lcd(0,"variable reistor");
 
-    DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x80,INTERNAL_HCLK , 1);     
-    
+       
+    DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x01,INTERNAL_HCLK , 1);     // changed from DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x80,INTERNAL_HCLK , 1);  
     while(1)
     {
     DrvADC_StartConvert();   // start A/D conversion

d8616afdeacebc48f9e5a5e58a2da7ab1d43ff9c
Gagan K Shetty <gagankshetty@gmail.com>
2018-04-18 14:10:02 +0530

    Update 4)Program for using ADC channel 6 and display analog value on the LCD.c

--- IOT/4)Program for using ADC channel 6 and display analog value on the LCD.c
+++ IOT/4)Program for using ADC channel 6 and display analog value on the LCD.c
@@ -20,12 +20,12 @@ int32_t main (void)
       clr_all_panel();  // clear LCD panel 
       print_lcd(0,"variable reistor");
 
-    DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x40,INTERNAL_HCLK , 1);     
+    DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x40,INTERNAL_HCLK , 1);     //make 0x40 -> 0x80 for channel 7
     while(1)
     {
 	    DrvADC_StartConvert();   // start A/D conversion
 	    while(DrvADC_IsConversionDone()==FALSE);
-	    value = ADC->ADDR[6].RSLT & 0xFFF;    
+	    value = ADC->ADDR[6].RSLT & 0xFFF;    //make value 6->7 for channel 7
 	    
 	    sprintf(TEXT,"Value: %d",value); // convert ADC0 value into text
 	    print_lcd(1, TEXT); // output TEXT to LCD

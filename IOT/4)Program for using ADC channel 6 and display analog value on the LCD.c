b97d14e3cdc2acd54e693e1a28b372f161717e68
Gagan K Shetty <gagankshetty@gmail.com>
2018-04-18 14:07:54 +0530

    Update 4)Program for using ADC channel 6 and display analog value on the LCD.c

--- IOT/4)Program for using ADC channel 6 and display analog value on the LCD.c
+++ IOT/4)Program for using ADC channel 6 and display analog value on the LCD.c
@@ -12,8 +12,9 @@ int32_t main (void)
     char TEXT[16];               
     
     UNLOCKREG();
-    SYSCLK->PWRCON.XTL12M_EN = 1;     //Enable 12Mhz and set HCLK->12Mhz
-    SYSCLK->CLKSEL0.HCLK_S = 0;
+    //SYSCLK->PWRCON.XTL12M_EN = 1;     //Enable 12Mhz and set HCLK->12Mhz
+    //SYSCLK->CLKSEL0.HCLK_S = 0;
+    DrvSYS_Open(48000000);
     LOCKREG();
     Initial_panel();  // initialize LCD pannel
       clr_all_panel();  // clear LCD panel 
